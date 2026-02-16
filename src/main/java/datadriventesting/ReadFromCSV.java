01c64ae77ab646033012bd4c510fd5a5ba725d5d
Yashwanth K <yashwanth.k@idexcel.com>
2026-02-16 20:35:51 +0530

    Practiced working with the opencsv library, added opencsv library into the pom.xml

---
+++ src/main/java/datadriventesting/ReadFromCSV.java
@@ -0,0 +1,41 @@
+package datadriventesting;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+public class ReadFromCSV {
+
+	public static void main(String[] args) throws IOException, CsvException {
+
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("TestData/LoginCreds.csv");
+		InputStreamReader reader = new InputStreamReader(is);
+		
+		/**
+		 * The below code, is not suggested to use - Hard Coding of file path
+		 * File file = new File("C:\\Users\\yashwanth.k\\OneDrive - Idexcel\\A SDET 2026\\Java-Rest Assured API Testing\\PhoenixApiAutomationFramework\\src\\main\\resources\\TestData\\LoginCreds.csv");
+		 * FileReader reader = new FileReader(file);
+		 */
+		
+		CSVReader csvRead = new CSVReader(reader);
+
+		List<String[]> dataList = csvRead.readAll();
+
+		for (String[] dataArray : dataList) {
+			System.out.println(Arrays.toString(dataArray));
+		}
+
+		long lines = csvRead.getLinesRead();
+		System.out.println(lines); // Returns number of Rows
+
+		long records = csvRead.getRecordsRead();
+		System.out.println(records);
+	}
+}
